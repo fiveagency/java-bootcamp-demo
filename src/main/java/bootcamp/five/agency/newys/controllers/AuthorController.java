01123f7004471de2f37eb3428bb1a0d94db93d02
Kristijan Salaj <kristijan.salaj@endava.com>
2022-08-15 13:40:53 +0200

    added new controller methods, renamed data.sql

--- src/main/java/bootcamp/five/agency/newys/controllers/AuthorController.java
+++ src/main/java/bootcamp/five/agency/newys/controllers/AuthorController.java
@@ -1,11 +1,15 @@
 package bootcamp.five.agency.newys.controllers;
 
-import bootcamp.five.agency.newys.dto.response.AuthorResponseDto;
+import bootcamp.five.agency.newys.domain.Author;
+import bootcamp.five.agency.newys.dto.response.author.GetAuthorDetailsResponseDto;
+import bootcamp.five.agency.newys.services.author.CreateAuthorService;
+import bootcamp.five.agency.newys.services.author.DeleteAuthorService;
 import bootcamp.five.agency.newys.services.author.GetAuthorService;
+import bootcamp.five.agency.newys.services.author.UpdateAuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,21 +17,48 @@ import java.util.List;
 public class AuthorController {
 
     private final GetAuthorService getAuthorService;
+    private final CreateAuthorService createAuthorService;
+
+    private final UpdateAuthorService updateAuthorService;
+
+    private final DeleteAuthorService deleteAuthorService;
 
     @Autowired
-    public AuthorController(GetAuthorService getAuthorService) {
+    public AuthorController(GetAuthorService getAuthorService, CreateAuthorService createAuthorService,
+                            UpdateAuthorService updateAuthorService, DeleteAuthorService deleteAuthorService) {
         this.getAuthorService = getAuthorService;
+        this.createAuthorService = createAuthorService;
+        this.updateAuthorService = updateAuthorService;
+        this.deleteAuthorService = deleteAuthorService;
     }
 
     @GetMapping("/authors")
-    public List<AuthorResponseDto> getAuthors() {
-        List<AuthorResponseDto> authorList = getAuthorService.getAll();
-        return authorList;
+    public ResponseEntity<List<GetAuthorDetailsResponseDto>> getAuthors() {
+        List<GetAuthorDetailsResponseDto> authorList = getAuthorService.getAll();
+        return new ResponseEntity<>(authorList, HttpStatus.OK);
     }
 
     @GetMapping("/author/{id}")
-    public AuthorResponseDto getAuthorById(@PathVariable Long id) {
-        AuthorResponseDto author = getAuthorService.getAuthorById(id);
-        return author;
+    public ResponseEntity<GetAuthorDetailsResponseDto> getAuthorById(@PathVariable Long id) {
+        GetAuthorDetailsResponseDto author = getAuthorService.getAuthorById(id);
+        return new ResponseEntity<>(author, HttpStatus.OK);
+    }
+
+    @PostMapping("/author/create")
+    public ResponseEntity<HttpStatus> createAuthor(@RequestBody Author author) {
+        createAuthorService.createAuthor(author.getFirstName(), author.getLastName(), author.getEmail(), author.getType());
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/author/{id}/update")
+    public ResponseEntity<HttpStatus> updateAuthor(@PathVariable Long id, @RequestBody Author author) {
+        updateAuthorService.updateAuthor(id, author.getFirstName(), author.getLastName(), author.getEmail(), author.getType());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/author/{id}/delete")
+    public ResponseEntity<HttpStatus> deleteAuthor(@PathVariable Long id) {
+        deleteAuthorService.deleteAuthorById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
